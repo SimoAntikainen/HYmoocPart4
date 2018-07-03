6ebbbdf93b75e24eec1877b29225e3dbd11d4ffe
Simo Antikainen <simo.antikainen@aalto.fi>
2018-07-03 21:22:48 +0300

    end of tutorial part 4

--- notebackend/controllers/notes.js
+++ notebackend/controllers/notes.js
@@ -1,4 +1,5 @@
 const notesRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Note = require('../models/note')
 const User = require('../models/user')
 
@@ -49,19 +50,51 @@ notesRouter.delete('/:id', (request, response) => {
       response.status(400).send({ error: 'malformatted id' })
     })
 })**/
-notesRouter.get('/:id', async (request, response) => {
-  try {
-    const note = await Note.findById(request.params.id)
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
-    if (note) {
-      response.json(Note.format(note))
-    } else {
-      response.status(404).end()
+notesRouter.post('/', async (request, response) => {
+  const body = request.body
+
+  try {
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
     }
 
-  } catch (exception) {
-    console.log(exception)
-    response.status(400).send({ error: 'malformatted id' })
+    if (body.content === undefined) {
+      return response.status(400).json({ error: 'content missing' })
+    }
+
+    const user = await User.findById(decodedToken.id)
+
+    const note = new Note({
+      content: body.content,
+      important: body.important === undefined ? false : body.important,
+      date: new Date(),
+      user: user._id
+    })
+
+    const savedNote = await note.save()
+
+    user.notes = user.notes.concat(savedNote._id)
+    await user.save()
+
+    response.json(Note.format(note))
+  } catch(exception) {
+    if (exception.name === 'JsonWebTokenError' ) {
+      response.status(401).json({ error: exception.message })
+    } else {
+      console.log(exception)
+      response.status(500).json({ error: 'something went wrong...' })
+    }
   }
 })
 
